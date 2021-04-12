ad10397391c7ba2d2ae588b565d39e8e495964e1
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2021-04-12 18:46:22 +0700

    copied files from private repo

---
+++ graph.js
@@ -0,0 +1,161 @@
+let myChart = document.getElementById('myChart').getContext('2d');
+var draggableChart = new Chart(myChart, {
+    data: {
+        datasets: [{
+            type: 'scatter',
+            data: [{x: 1,y: 1}, {x: 2,y: 2}, {x: 3,y: 3}, {x: 4,y: 4}, {x: 5,y: 5}],
+            fill: false,
+            tension: 0.4,
+            borderWidth: 1,
+            pointRadius: 4.5, 
+            pointHitRadius: 10, // for improved touch support
+            borderColor: '#FF0000',
+            label: 'Data Point'
+        },{
+            type: 'line',
+            data: [{x: 0,y: 0}, {x: 0,y: 0}],
+            dragData: false, // prohibit dragging this dataset
+            pointRadius: 0,
+            label: 'Regression Line'
+        },{
+            hidden: false,
+            type: 'scatter',
+            data: [{x: 11,y: 11}, {x: 12,y: 12}, {x: 13,y: 13}, {x: 14,y: 14}, {x: 15,y: 15}],
+            fill: false,
+            tension: 0.4,
+            borderWidth: 1,
+            pointRadius: 4.5, 
+            pointHitRadius: 10, // for improved touch support
+            borderColor: '#0000FF' 
+        },{
+            name: 'knnPointer',
+            type: 'scatter',
+            pointRadius: 4.5,
+            data: [],
+            dragData: false
+        },{
+            name: 'knnLine1',
+            type: 'line',
+            data: [{x: 0,y: 0}, {x: 0,y: 0}],
+            dragData: false,
+            pointRadius: 0,
+            borderWidth: 1
+        },{
+            name: 'knnLine2',
+            type: 'line',
+            data: [{x: 0,y: 0}, {x: 0,y: 0}],
+            dragData: false,
+            pointRadius: 0,
+            borderWidth: 1
+        },{
+            name: 'knnLine3',
+            type: 'line',
+            data: [{x: 0,y: 0}, {x: 0,y: 0}],
+            dragData: false,
+            pointRadius: 0,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                max: 25,
+                min: 0,
+                // dragData: false // disables datapoint dragging for the entire axis
+            },
+            x: {
+                dragData: true,
+                type: 'linear',
+                position: 'bottom',
+                max: 25,
+                min: 0
+            }
+        },
+        onClick: function graphClick(e, element){
+            let canvasPosition = Chart.helpers.getRelativePosition(e, draggableChart);
+            let x = draggableChart.scales.x.getValueForPixel(canvasPosition.x);
+            let y = draggableChart.scales.y.getValueForPixel(canvasPosition.y);
+            console.log(x, y);
+            if(element.length > 0){
+                lastClickedElement = element[0];
+                let dataX = document.getElementById('dataX');
+                let dataY = document.getElementById('dataY');
+                let datasetIndex = lastClickedElement.datasetIndex;
+                let index = lastClickedElement.index;
+
+                if(clickMode === 'delete'){
+                    if (index > -1) {
+                        draggableChart.data.datasets[datasetIndex].data.splice(index, 1);
+                        if(datasetIndex === 3){
+                            draggableChart.data.datasets[4].data = [];
+                            draggableChart.data.datasets[5].data = [];
+                            draggableChart.data.datasets[6].data = [];
+                        }
+                        draggableChart.update();
+                    }
+                }
+                else{
+                    dataX.value = draggableChart.data.datasets[datasetIndex].data[index].x.toFixed(1);
+                    dataY.value = draggableChart.data.datasets[datasetIndex].data[index].y.toFixed(1);
+                }
+
+            }
+            else{
+                dataX.value = 'NaN';
+                dataY.value = 'NaN';
+                lastClickedElement = NaN;
+
+                if(clickMode == 'drawRed'){
+                    addData(0, x, y);
+                }
+                else if(clickMode == 'drawBlue'){
+                    addData(2, x, y);
+                }
+                else if(clickMode == 'KNN'){
+                    knn(x, y);
+                }
+            }
+        },
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip:{
+                enabled: false
+            },
+            dragData: {
+                round: 1, // rounds the values to n decimal places 
+                // in this case 1, e.g 0.1234 => 0.1)
+                showTooltip: true, // show the tooltip while dragging [default = true]
+                dragX: true, // also enable dragging along the x-axis.
+                // this solely works for continous, numerical x-axis scales (no categories or dates)!
+                onDragStart: function (e, element) {
+                    /*
+                    // e = event, element = datapoint that was dragged
+                    // you may use this callback to prohibit dragging certain datapoints
+                    // by returning false in this callback
+                    if (element.datasetIndex === 0 && element.index === 0) {
+                        // this would prohibit dragging the first datapoint in the first
+                        // dataset entirely
+                        return false
+                    }
+                    */
+                },
+                onDrag: function (e, datasetIndex, index, value) {
+                    /*     
+                    // you may control the range in which datapoints are allowed to be
+                    // dragged by returning `false` in this callback
+                    if (value < 0) return false // this only allows positive values
+                    if (datasetIndex === 0 && index === 0 && value > 20) return false 
+                    */
+                },
+                onDragEnd: function (e, datasetIndex, index, value) {
+                    // you may use this callback to store the final datapoint value
+                    // (after dragging) in a database, or update other UI elements that
+                    // dependent on it
+                },
+            }
+        }
+    }
+})
---
+++ logic.js
@@ -0,0 +1,203 @@
+var rawColumns = [];
+var rawTable = [];
+var regressionLine = {m:0, b:0};
+var lastClickedElement = NaN;
+var clickMode = 'view';
+var k = 3;
+
+function getInput(){
+    let x = document.getElementById('inputX').value;
+    let y = document.getElementById('inputY').value;
+    console.log(x, y);
+    addData(0, x, y);
+}
+
+function addData(i, x, y){
+    draggableChart.data.datasets[i].data.unshift({x: x, y: y});
+    draggableChart.update();
+}
+
+function drawModeRed(){
+    document.getElementById('mode').textContent = "Draw Mode Red";
+    clickMode = 'drawRed';
+}
+
+function drawModeBlue(){
+    document.getElementById('mode').textContent = "Draw Mode Blue";
+    clickMode = 'drawBlue';
+}
+
+function knnMode(){
+    document.getElementById('mode').textContent = "KNN Mode";
+    clickMode = 'KNN';
+}
+
+function deleteMode(){
+    document.getElementById('mode').textContent = "Delete Mode";
+    clickMode = 'delete';
+}
+
+function viewMode(){
+    document.getElementById('mode').textContent = "View Mode";
+    clickMode = 'view';
+}
+
+function clearGraph(){
+    draggableChart.data.datasets[0].data.splice(0, draggableChart.data.datasets[0].data.length);
+    draggableChart.data.datasets[1].data.splice(0, draggableChart.data.datasets[1].data.length);
+    draggableChart.update();
+}
+
+function clearRegression(){
+    draggableChart.data.datasets[1].data.splice(0, draggableChart.data.datasets[1].data.length);
+    draggableChart.update();
+}
+
+function linearRegression(){
+    let m = 1;
+    let b = 0;
+    let x1 = 0;
+    let y1 = m * x1 + b;
+    let x2 = 1;
+    let y2 = m * x2 + b;
+
+    let dataLength = draggableChart.data.datasets[0].data.length;
+    let xSum = 0;
+    let ySum = 0;
+    for(let i = 0; i < dataLength; i++){
+        let data = draggableChart.data.datasets[0].data[i];
+        xSum += data.x;
+        ySum += data.y;
+    }
+    let xMean = xSum / dataLength;
+    let yMean = ySum / dataLength;
+
+    let numerator = 0;
+    let denominator = 0;
+    for(let i = 0; i < dataLength; i++){
+        let data = draggableChart.data.datasets[0].data[i];
+        numerator += (data.x - xMean) * (data.y - yMean);
+        denominator += (data.x - xMean) * (data.x - xMean);
+    }
+
+    m = numerator/denominator;
+    b = yMean - m * xMean;
+
+    x1 = 0;
+    y1 = m * x1 + b;
+    x2 = 25;
+    y2 = m * x2 + b;
+
+    draggableChart.data.datasets[1].data[0] = {x: x1, y: y1};
+    draggableChart.data.datasets[1].data[1] = {x: x2, y: y2};
+    draggableChart.data.datasets[1].pointRadius = 0;
+    draggableChart.update();
+
+    regressionLine.m = m;
+    regressionLine.b = b;
+}
+
+function knnSort(a, b){
+    if(a.dist > b.dist) return 1;
+    if(a.dist < b.dist) return -1;
+    return 0;
+}
+
+function knnLine(x, y, arr){
+    let line1 = draggableChart.data.datasets[4];
+    let line2 = draggableChart.data.datasets[5];
+    let line3 = draggableChart.data.datasets[6];
+
+    line1.borderColor = arr[0].backgroundColor;
+    line2.borderColor = arr[1].backgroundColor;
+    line3.borderColor = arr[2].backgroundColor;
+
+    line1.data[0] = {x: x, y: y};
+    line2.data[0] = {x: x, y: y};
+    line3.data[0] = {x: x, y: y};
+
+    line1.data[1] = {x: arr[0].x, y: arr[0].y};
+    line2.data[1] = {x: arr[1].x, y: arr[1].y};
+    line3.data[1] = {x: arr[2].x, y: arr[2].y};
+}
+
+function knn(x, y){
+    let data1 = draggableChart.data.datasets[0];
+    let data2 = draggableChart.data.datasets[2];
+    let dataLength1 = data1.data.length;
+    let dataLength2 = data2.data.length;
+
+    let distData = [];
+
+    for(let i = 0; i < dataLength1; i++){
+        let dist = Math.sqrt(Math.pow((data1.data[i].x - x),2)+Math.pow((data1.data[i].y - y),2));
+        let distObject = {category: 0, dist: dist, x: data1.data[i].x, y: data1.data[i].y, backgroundColor: data1.borderColor};
+        distData.unshift(distObject);
+    }
+    for(let i = 0; i < dataLength2; i++){
+        let dist = Math.sqrt(Math.pow((data2.data[i].x - x),2)+Math.pow((data2.data[i].y - y),2));
+        let distObject = {category: 1, dist: dist, x: data2.data[i].x, y: data2.data[i].y, backgroundColor: data2.borderColor};
+        distData.unshift(distObject);
+    }
+
+    distData.sort(knnSort);
+    console.log(distData);
+
+    let neighbors = [0, 0];
+    let maxDist = 0;
+    for(let i = 0; i < k; i++){
+        neighbors[distData[i].category] += 1;
+        if(distData[i].dist > maxDist) maxDist = distData[i].dist;
+    }
+    console.log(neighbors);
+    console.log(maxDist);
+
+    if(draggableChart.data.datasets[3].data.length < 1){
+        addData(3, x, y);
+        if(neighbors[0] > neighbors[1]){
+            draggableChart.data.datasets[3].backgroundColor = '#FF0000';
+        }
+        else{
+            draggableChart.data.datasets[3].backgroundColor = '#0000FF';
+        }
+    }
+    else{
+        draggableChart.data.datasets[3].data[0].x = x;
+        draggableChart.data.datasets[3].data[0].y = y;
+        if(neighbors[0] > neighbors[1]){
+            draggableChart.data.datasets[3].backgroundColor = '#FF0000';
+        }
+        else{
+            draggableChart.data.datasets[3].backgroundColor = '#0000FF';
+        }
+    }
+    knnLine(x, y, distData.slice(0,3));
+    draggableChart.update();
+}
+
+var modal = document.getElementsByClassName("modal")[0];
+
+function showModal(){
+    modal.style.display = "block";
+}
+
+function closeModal() { 
+    modal.style.display = "none";
+}
+
+function upload() {
+    modal.style.display = "none";
+    console.log(document.getElementById("input").files[0]);
+    var FR = new FileReader();
+    FR.readAsText(document.getElementById("input").files[0]);
+    FR.onloadend = function() {
+        console.log("Finished Reading", FR.readyState);
+        var lines = FR.result.split("\n");
+        rawColumns = lines[0].split(",");
+        for(let i = 1; i < lines.length; i++) {
+            rawTable.push(lines[i].split(","));
+        }
+        // console.log(FR.result.split("\n"));
+        console.log(rawTable);
+    };
+}
